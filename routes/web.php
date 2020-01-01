411b31e7c5ec2f65272d31eea2c9b0bbf055551f
Adebayo Olaonipekun <adebayool@armpension.com>
2020-01-01 22:02:40 +0100

    one to one reltionship

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
+use App\User;
+use App\Address;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert/{id}', function($id){
+
+    $user = User::findOrFail($id);
+
+    $address = new Address(['name' => '30 Akute-Ajuwon Ifo LGA']);
+
+    $user->address()->save($address);
+
+});
+
+Route::get('/update/{id}', function($id){
+    
+    $address = Address::where('user_id', $id)->first();
+
+    $address->name = "2345 Updated Avenue Liverpool, UK";
+
+    $address->save();
+
+});
+
+Route::get('/read/{id}', function($id){
+
+    $user = User::findOrFail($id);
+
+    echo $user->address->name;
+
+});
+
+Route::get('/delete/{id}', function($id){
+
+    $user = User::findOrFail($id);
+
+    $user->address()->delete();
+
+    return "done";
+
 });
